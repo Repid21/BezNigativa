a8caf1434b77c632308ec429bdf5bfa00b5e1486
Repid21 <repidtop@gmail.com>
2026-08-31 11:30:13 +0300

    прикрепил ошибку

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
--- BezNigativa v9.4 injector entrypoint.
+-- BezNigativa v9.5 injector entrypoint.
 local BASE_URL = "https://raw.githubusercontent.com/Repid21/BezNigativa/main/src/"
-local BUILD_QUERY = "?build=9.4"
+local BUILD_QUERY = "?build=9.5"
 local cache = {}
 
 local function loadModule(name)
